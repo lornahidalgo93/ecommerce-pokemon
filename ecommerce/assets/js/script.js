86d150afd1da4b68ee838f9bb88e7970e9e3679b
Lorna Hidalgo <lornahidalgobarria@gmail.com>
2026-03-16 16:02:01 -0300

    botones funcionales

--- ecommerce/assets/js/script.js
+++ ecommerce/assets/js/script.js
@@ -1,7 +1,31 @@
 let contador= 0;
+const elige =document.querySelector("#eligePokemon");
+const elige2 =document.querySelector("#pokemones");
+const contact =document.querySelector("#contacto");
 
 function agregarProducto(){
     contador++;
     
    $("#pokebolas").text(contador);
 }
+elige.addEventListener("click", ()=>{
+    
+    window.scrollTo({
+        top:500,
+        behavior:"smooth",
+    });
+});
+elige2.addEventListener("click", ()=>{
+    
+    window.scrollTo({
+        top:500,
+        behavior:"smooth",
+    });
+});
+contact.addEventListener("click", ()=>{
+    
+    window.scrollTo({
+        top:5000,
+        behavior:"smooth",
+    });
+});
